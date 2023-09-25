d656b40d1bc5d38a5ebb9e5c17b2f301c1669ca5
petrostrak <pit.trak@gmail.com>
2023-09-25 17:07:40 +0300

    instantiate users_service and sessions_service

--- src/auth-service/main.rs
+++ src/auth-service/main.rs
@@ -5,18 +5,21 @@ mod sessions;
 mod users;
 
 use auth::*;
-use sessions::{SessionsImpl, Sessions};
-use users::{UsersImpl, Users};
+use sessions::{Sessions, SessionsImpl};
+use users::{Users, UsersImpl};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces. This is needed for Docker to work, which we will add later on.
+    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
+    // This is needed for Docker to work, which we will add later on.
     // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
     // Port 50051 is the recommended gRPC port.
     let addr = "[::0]:50051".parse()?;
 
-    let users_service: Box<Mutex<dyn Users + Send + Sync + 'static>> = todo!(); // Create user service instance
-    let sessions_service: Box<Mutex<dyn Sessions + Send + Sync + 'static>> = todo!(); //Create session service instance
+    let users_service: Box<Mutex<dyn Users + Send + Sync + 'static>> =
+        Box::new(Mutex::new(UsersImpl::default()));
+    let sessions_service: Box<Mutex<dyn Sessions + Send + Sync + 'static>> =
+        Box::new(Mutex::new(SessionsImpl::default()));
 
     let auth_service = AuthService::new(users_service, sessions_service);
 
